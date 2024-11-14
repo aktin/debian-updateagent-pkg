fa021c49486bf035d70cfbff9eb81d23710e84d7
Alexander Kombeiz <alexkombie@gmail.com>
2024-11-14 10:55:42 +0100

    added metadata and refactored build.sh and clean.sh

--- src/clean.sh
+++ src/clean.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-rm -rf debian/build
+#--------------------------------------
+# Script Name:  clean.sh
+# Version:      1.0
+# Authors:      skurka@ukaachen.de, akombeiz@ukaachen.de
+# Date:         14 Nov 24
+# Purpose:      Cleans up update agent Debian package build directories.
+#--------------------------------------
+
+set -euo pipefail
+
+dir_current="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+rm -rf "${dir_current}/build"
