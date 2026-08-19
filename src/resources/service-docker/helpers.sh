f0c9c095d09fcebfa5b87cea300f917a977e6600
WHoy <wiliam.hoy@alumni.fh-aachen.de>
2026-08-19 14:54:22 +0200

    testing new logger logic

--- src/resources/service-docker/helpers.sh
+++ src/resources/service-docker/helpers.sh
@@ -8,9 +8,8 @@
 #--------------------------------------
 
 
-function log() {
-  local message="$1"
-  echo "[LOGGING] tenant=${dwh_prefix:-unknown} $message" >&2
+log() {
+  logger -t "${PACKAGE_NAME}" -p user.info -- "$1" 2>/dev/null || true
 }
 
 # Get version of last released data warehouse, from the AKTIN Github rspository
